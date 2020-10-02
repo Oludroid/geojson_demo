7f1110870e29ddcab91722c71befb6d05f6897ec
Oludroid <72241257+Oludroid@users.noreply.github.com>
2020-10-02 02:50:55 -0500

    Update geojson

--- geojson.js
+++ geojson.js
@@ -6,107 +6,42 @@
 //
 //    See example below:
 
+
 var json = {
-  "type": "FeatureCollection",
+"type": "FeatureCollection",
   "features": [
     {
       "type": "Feature",
       "properties": {
-        "stroke": "#555555",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "fill": "#555555",
-        "fill-opacity": 0.5,
-        "image": "https://1.bp.blogspot.com/-WbftUEch0t0/X2DQmPdyksI/AAAAAAAAAIs/JHH1hwcqN0IVWsR8KCYabom1fqoweHBFACLcBGAsYHQ/w1684-h1069-p-k-no-nu/hearts.jpg",
-        "title": "Nebraska State Capitol",
-        "description": "Some description about something that happened at the capitol"
-      },
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              -96.70105934143066,
-              40.806971850092815
-            ],
-            [
-              -96.69826984405518,
-              40.806971850092815
-            ],
-            [
-              -96.69826984405518,
-              40.809131953785965
-            ],
-            [
-              -96.70105934143066,
-              40.809131953785965
-            ],
-            [
-              -96.70105934143066,
-              40.806971850092815
-            ]
-          ]
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#ee05ff",
+        "marker-color": "#6b1de0",
         "marker-size": "medium",
         "marker-symbol": "",
-        "ingredients": "rhubarb, strawberries, egg, milk, butter, so much sugar",
-        "thanks": "thanks to Linnea for giving me a pie",
-        "title": "Starting Point",
-        "image": "https://s3.amazonaws.com/omeka-net/53984/archive/square_thumbnails/e5847940c8baa5e9e50ad71ee468ba0e.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1600905600&Signature=PapYafLVt%2BGfMiI%2F06Ibpay%2FXPk%3D"
+        "Title": "Q & 14th St (Start)",
+        "Image": "file:///C:/Users/oluwa/AppData/Local/Temp/IMG_20200924_171914.jpg",
+        "Description": "\"Birds\" at the start",
+        "Date": "22/9/2020"
       },
       "geometry": {
         "type": "Point",
         "coordinates": [
-          -96.70299053192137,
-          40.81015513625622
+          -96.7011746764183,
+          40.8158422409101
         ]
       }
     },
     {
       "type": "Feature",
-      "properties": {
-        "stroke": "#ff5100",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "title": "Path to the library",
-        "image": "https://1.bp.blogspot.com/-WbftUEch0t0/X2DQmPdyksI/AAAAAAAAAIs/JHH1hwcqN0IVWsR8KCYabom1fqoweHBFACLcBGAsYHQ/w1684-h1069-p-k-no-nu/hearts.jpg"
-      },
+      "properties": {},
       "geometry": {
         "type": "LineString",
         "coordinates": [
           [
-            -96.69979333877563,
-            40.80922127986828
+            -96.70849308371544,
+            40.81586152528306
           ],
           [
-            -96.69986844062804,
-            40.811389430620544
-          ],
-          [
-            -96.6997504234314,
-            40.811405671182825
-          ],
-          [
-            -96.6996967792511,
-            40.81468618325975
-          ],
-          [
-            -96.69984698295592,
-            40.8146943031379
-          ],
-          [
-            -96.69984698295592,
-            40.816837916207085
-          ],
-          [
-            -96.70262575149535,
-            40.81687851427114
+            -96.70118004083633,
+            40.81583615110696
           ]
         ]
       }
@@ -114,64 +49,199 @@ var json = {
     {
       "type": "Feature",
       "properties": {
-        "stroke": "#d4ff00",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "fill": "#0060ff",
-        "fill-opacity": 0.2,
-        "title": "Love Library",
-        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/189753833@N07/50237618773/in/album-72157715528007683/\" title=\"yawn_connie\"><img src=\"https://live.staticflickr.com/65535/50237618773_c9052299b5_w.jpg\" width=\"300\" height=\"400\" alt=\"yawn_connie\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>",
-        "random": "Some other random field"
+        "marker-color": "#220bf2",
+        "marker-size": "medium",
+        "marker-symbol": ""
       },
       "geometry": {
-        "type": "Polygon",
+        "type": "Point",
+        "coordinates": [
+          -96.70849442481995,
+          40.81587167495076
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
         "coordinates": [
           [
-            [
-              -96.70274376869202,
-              40.817040906278805
-            ],
-            [
-              -96.70216441154479,
-              40.81700030831414
-            ],
-            [
-              -96.70187473297119,
-              40.817235776163265
-            ],
-            [
-              -96.70193910598755,
-              40.81753619944697
-            ],
-            [
-              -96.70217514038086,
-              40.81769858984467
-            ],
-            [
-              -96.70218586921692,
-              40.818104564099706
-            ],
-            [
-              -96.70297980308533,
-              40.81808832517719
-            ],
-            [
-              -96.70300126075745,
-              40.817609275175144
-            ],
-            [
-              -96.70323729515076,
-              40.81742252593206
-            ],
-            [
-              -96.70299053192137,
-              40.81704902586876
-            ],
-            [
-              -96.70274376869202,
-              40.817040906278805
-            ]
+            -96.70996427536011,
+            40.815863555216715
+          ],
+          [
+            -96.70856952667235,
+            40.81585543548167
           ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#3914e9",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Title": "Tipsy Trina's Cantina"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70996427536011,
+          40.81589603414695
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -96.71185255050658,
+            40.81478362173202
+          ],
+          [
+            -96.71353697776794,
+            40.81475926212737
+          ],
+          [
+            -96.71180963516235,
+            40.81480798132771
+          ],
+          [
+            -96.71185255050658,
+            40.81592039333422
+          ],
+          [
+            -96.70968532562256,
+            40.81591227360612
+          ],
+          [
+            -96.70995354652405,
+            40.81589603414695
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#3e3ac2",
+        "marker-size": "medium",
+        "marker-symbol": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.71286106109618,
+          40.81479986146346
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -96.71191692352295,
+            40.81258310135661
+          ],
+          [
+            -96.71178817749023,
+            40.81259934162669
+          ],
+          [
+            -96.7118740081787,
+            40.81258310135661
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -96.71186327934265,
+            40.81470648295327
+          ],
+          [
+            -96.71188473701477,
+            40.81251002009193
+          ],
+          [
+            -96.70700311660767,
+            40.81251002009193
+          ],
+          [
+            -96.70701384544373,
+            40.811381310337914
+          ],
+          [
+            -96.70557618141174,
+            40.81143815229546
+          ],
+          [
+            -96.70555472373962,
+            40.81082912878771
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#3037cd",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "Title": "TGroup Thrapy Bike Tours",
+        "Description": "The end of the walk"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70556545257568,
+          40.810934693262894
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -96.70404195785522,
+            40.81583107627058
+          ],
+          [
+            -96.70404195785522,
+            40.813646830651216
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#2533d8",
+        "marker-size": "medium",
+        "marker-symbol": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70404195785522,
+          40.81367931066668
         ]
       }
     }
